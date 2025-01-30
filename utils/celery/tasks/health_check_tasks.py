91816cd4600b31599555f7ffc22578db29589a2b
krishnareddy <tkcreddy@yahoo.com>
2025-01-29 19:24:07 -0500

    updated main_aws with tag and cluster information.

--- utils/celery/tasks/health_check_tasks.py
+++ utils/celery/tasks/health_check_tasks.py
@@ -4,10 +4,13 @@ import asyncio
 from utils.celery.celery_config import celery_app
 from celery import shared_task,Task
 from datetime import datetime
+from time import time
+from utils.redis.hc_track import HcTrack
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 # Configure logging
 from logpkg.log_kcld import LogKCld, log_to_file
+from utils.redis.hc_failure_tracker import HcFailureTracker
 
 logger = LogKCld()
 
@@ -19,64 +22,6 @@ class AsyncTask(Task):
 
 # List of URLs to check
 
-@celery_app.task
-@shared_task(bind=True)
-@log_to_file(logger)
-def perform_health_check(self, urls, concurrency_limit=30):
-    """
-    Perform health checks for a given list of URLs with concurrency control.
-    """
-
-    async def check_url(url):
-        try:
-            ct = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            async with httpx.AsyncClient(timeout=5) as client:
-                logger.info(f"Checking: {url}")
-                response = await client.get(url)
-                return {'current time': ct, 'url': url, 'status': 'healthy' if response.status_code == 200 else 'unhealthy', 'status_code': response.status_code}
-        except httpx.RequestError as exc:
-            logger.error(f"Failed: {url} -> {exc}")
-            return {'url': url, 'status': 'unhealthy', 'status_code': None, 'error': str(exc)}
-
-    async def schedule_checks(urls, concurrency_limit):
-        semaphore = asyncio.Semaphore(concurrency_limit)
-
-        async def limited_check(url):
-            async with semaphore:
-                return await check_url(url)
-
-        # Schedule checks for the provided URLs
-        results = [await limited_check(url) for url in urls]
-        return results
-
-    # Use asyncio.run to execute the coroutine
-    return asyncio.run(schedule_checks(urls, concurrency_limit))
-
-
-@celery_app.task
-@shared_task(bind=True)
-@log_to_file(logger)
-def perform_http_check(urls):
-    results = []
-
-    def health_check(url):
-        try:
-            ct = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            logger.info(f"Checking: {url}")
-            response = requests.get(url, timeout=5)
-            return {'current time': ct, 'url': url, 'status': 'healthy' if response.status_code == 200 else 'unhealthy',
-                    'status_code': response.status_code}
-        except requests.RequestException as exc:
-            logger.error(f"Failed: {url} -> {exc}")
-            return {'url': url, 'status': 'unhealthy', 'status_code': None, 'error': str(exc)}
-
-    # Perform health checks with threading
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        print(f"Urls for check are {urls}")
-        futures = {executor.submit(health_check, url): url for url in urls}
-        for future in as_completed(futures):
-            results.append(future.result())
-    return results
 
 
 # Function to perform a health check
@@ -89,16 +34,20 @@ async def health_check_task(urls, max_concurrency=100):
     async def health_check(url, session):
         try:
             ct = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            current_time=time()
             logger.info(f"Checking: {url}")
+            ht=HcTrack(host='localhost', port=6379,db=1)
             async with session.get(url, timeout=5,allow_redirects=False,verify_ssl=False) as response:
                 print(str(response.status))
                 if response.status == 200:
                     status='healthy'
+                    ht.track_consecutive_failures(url, status, current_time)
                 else:
                     status='unhealthy'
+                    ht.track_consecutive_failures(url,status,current_time)
                 logger.info(f"current time: {ct}, url: {url}, status: {status},status_code: {response.status}")
-                return {'current time': ct, 'url': url, 'status': 'healthy' if response.status == 200 else 'unhealthy',
-                    'status_code': response.status}
+
+                return {'current time': ct, 'url': url, 'status': status, 'status_code': response.status}
         except Exception as exc:
             logger.info(f"current time: {ct}, url: {url}, status: 600,status_code: unhealthy error: {str(exc)}")
             return {'url': url, 'status': 'unhealthy', 'status_code': None, 'error': str(exc)}
