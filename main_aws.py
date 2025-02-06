ddf737da84ec526881d0d730b3aa081cbe079aed
krishnareddy <tkcreddy@yahoo.com>
2025-02-05 22:57:02 -0500

    update aws celery startup, posting to aws queues for testing and redis_interface.py updated

--- main_aws.py
+++ main_aws.py
@@ -3,6 +3,7 @@ from logpkg.log_kcld import LogKCld
 from utils.celery.celery_config import celery_app
 from utils.extensions.utilities_extention import UtilitiesExtension
 from kombu import Queue,Exchange
+from  utils.redis.redis_interface import RedisInterface
 import time
 logger = LogKCld()
 from utils.celery.tasks.aws_tasks import get_ec2_instances,create_worker_nodes,terminate_worker_node
@@ -17,9 +18,9 @@ def main():
     #secure_exchange = Exchange('secure_exchange', type='direct')
     aws_config = read_config.aws_config
     ue=UtilitiesExtension(key_read['key'])
-    print(f"print {aws_config}")
-    #aws_interface_queue_name=
-    queue_info={}
+    redis_db_config=read_config.redis_db_config
+    rd=RedisInterface(redis_db_config['redis_host'],redis_db_config['redis_port'],redis_db_config['redis_db'])
+
     queue_info={'exchange': Exchange('secure_exchange', type='direct'), 'queue': ue.encode_hostname_with_key('aws_interface'), 'routing_key': ue.encode_hostname_with_key('aws_interface'), 'delivery_mode': 2}
 
     # result=get_ec2_instances.apply_async(
@@ -27,37 +28,37 @@ def main():
     #
     # print("Task sent. Waiting for result...")
     # print(result.get(timeout=30))
-    min=2
-    max=2
-    cluster_key='ClusterName'
-    cluster_value='Test_Cluster'
+    namespace="testCluster"
+    min:int=2
+    max:int=2
+
     min_max_tags={
         'MinCount':min,
-        'MaxCount':max,
-        'TagSpecifications':[{
-            'ResourceType': 'instance',
-            'Tags': [
-                    {
-                    'Key': cluster_key,
-                    'Value': cluster_value
-                    }
-            ]
-            }]
+        'MaxCount':max
     }
-    # result = create_worker_nodes.apply_async(args=(aws_config['aws_access_key_id'], aws_config['aws_secret_access_key'], aws_config['region'],instance_type,ami_id,key_name,security_group_ids),
+
+    #### Create instances
+    # result = create_worker_nodes.apply_async(args=(aws_config['aws_access_key_id'], aws_config['aws_secret_access_key'], aws_config['region'],instance_type,ami_id,key_name,security_group_ids,namespace),
     #                                          kwargs=min_max_tags,**queue_info)
     # print("Task sent. Waiting for result...")
     # response= result.get(timeout=30)
-    # instances=[]
+    # instances={}
     # print(min_max_tags['MaxCount'])
+    # print(response)
     # for i in range(int(min_max_tags['MaxCount'])):
-    #     instances.append(response['Instances'][i]['InstanceId'])
+    #     instances[response['Instances'][i]['PrivateDnsName']]={'IpAddress':response['Instances'][i]['PrivateIpAddress'],'InstanceId':response['Instances'][i]['InstanceId'],'NameSpace':namespace,'InstanceType':response['Instances'][i]['InstanceType']}
     # print(instances)
-    #time.sleep(50)
-    instances=['i-07247997712a85fc7', 'i-0611d52dfc54bddec']
-    result = terminate_worker_node.apply_async(args=(aws_config['aws_access_key_id'], aws_config['aws_secret_access_key'], aws_config['region'],instances),**queue_info)
-    response = result.get(timeout=30)
+    # for k,v in instances.items():
+    #     rd.save_node(k,v)
 
+    #time.sleep(50)
+
+    ### Terminate nodes
+    instances_to_terminate=rd.get_instance_ids_namespace(namespace)
+    print(instances_to_terminate)
+    result = terminate_worker_node.apply_async(args=(aws_config['aws_access_key_id'], aws_config['aws_secret_access_key'], aws_config['region'],instances_to_terminate),**queue_info)
+    response = result.get(timeout=30)
+    print(response)
 #print(str(response))
 
 
